3a0a51f018a7a5c10f4836ceda5a75ed49cd73e6
Artem <rubikkubika@gmail.com>
2025-10-12 05:48:01 +0300

    Fix nginx DNS resolution issue by using variables in proxy_pass

--- frontend-react/src/components/CommitInfo.tsx
+++ frontend-react/src/components/CommitInfo.tsx
@@ -8,7 +8,7 @@ interface CommitInfoProps {
 
 const CommitInfo: React.FC<CommitInfoProps> = ({ className }) => {
   // Hardcoded commit info - update this with each commit/PR to main
-  const commitHash = "m8k3n";
+  const commitHash = "n9x2k";
 
   const shortCommit = commitHash.substring(0, 5);
 
