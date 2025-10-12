782083662c8409067519f70c266aea60714bfe79
Artem <rubikkubika@gmail.com>
2025-10-12 05:46:57 +0300

    Add ultra-aggressive nginx timeout settings to prevent restarts

--- backend/src/main/java/com/dobalito/controller/AppController.java
+++ backend/src/main/java/com/dobalito/controller/AppController.java
@@ -32,7 +32,7 @@ public class AppController {
     @GetMapping("/info/commit")
     public ResponseEntity<?> commitInfo() {
         return ResponseEntity.ok(Map.of(
-            "commit", "p4x7q"
+            "commit", "m8k3n"
         ));
     }
 }
