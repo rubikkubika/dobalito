a30817ce34e8ed29e11cc04ae25223fe22cacd4c
Artem <rubikkubika@gmail.com>
2025-10-12 05:43:43 +0300

    Fix nginx restarting issue with aggressive timeout settings

--- frontend-react/src/components/CommitInfo.tsx
+++ frontend-react/src/components/CommitInfo.tsx
@@ -8,7 +8,7 @@ interface CommitInfoProps {
 
 const CommitInfo: React.FC<CommitInfoProps> = ({ className }) => {
   // Hardcoded commit info - update this with each commit/PR to main
-  const commitHash = "k9m2x";
+  const commitHash = "p4x7q";
 
   const shortCommit = commitHash.substring(0, 5);
 
