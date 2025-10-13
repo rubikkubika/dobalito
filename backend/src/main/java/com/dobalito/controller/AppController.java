3523b6a985726e21719e9f6ff0b91ec85b098be5
Artem <rubikkubika@gmail.com>
2025-10-13 23:36:27 +0300

    Update commit version to UKLHw

--- backend/src/main/java/com/dobalito/controller/AppController.java
+++ backend/src/main/java/com/dobalito/controller/AppController.java
@@ -32,7 +32,7 @@ public class AppController {
     @GetMapping("/info/commit")
     public ResponseEntity<?> commitInfo() {
         return ResponseEntity.ok(Map.of(
-            "commit", "po23t"
+            "commit", "UKLHw"
         ));
     }
 }
--- frontend-react/src/components/CommitInfo.tsx
+++ frontend-react/src/components/CommitInfo.tsx
@@ -8,7 +8,7 @@ interface CommitInfoProps {
 
 const CommitInfo: React.FC<CommitInfoProps> = ({ className }) => {
   // Hardcoded commit info - update this with each commit/PR to main
-  const commitHash = "po23t";
+  const commitHash = "UKLHw";
 
   const shortCommit = commitHash.substring(0, 5);
 
