628e510f609423a2072561f7e6de8d14c744f94e
Artem <rubikkubika@gmail.com>
2025-10-14 18:12:23 +0300

    Update commit version to K8mN3

--- backend/src/main/java/com/dobalito/controller/AppController.java
+++ backend/src/main/java/com/dobalito/controller/AppController.java
@@ -32,7 +32,7 @@ public class AppController {
     @GetMapping("/info/commit")
     public ResponseEntity<?> commitInfo() {
         return ResponseEntity.ok(Map.of(
-            "commit", "P7nQ4"
+            "commit", "K8mN3"
         ));
     }
 }
