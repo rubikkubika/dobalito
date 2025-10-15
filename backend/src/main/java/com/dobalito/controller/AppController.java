21c73f35b4b9758f4a047f65af238a87b9b8b344
Artem <rubikkubika@gmail.com>
2025-10-15 05:01:32 +0300

    Implement Docker registry hash-based build optimization

--- backend/src/main/java/com/dobalito/controller/AppController.java
+++ backend/src/main/java/com/dobalito/controller/AppController.java
@@ -32,7 +32,7 @@ public class AppController {
     @GetMapping("/info/commit")
     public ResponseEntity<?> commitInfo() {
         return ResponseEntity.ok(Map.of(
-            "commit", "eec88e"
+            "commit", "cd26c5"
         ));
     }
 }
