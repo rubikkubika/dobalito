abe2735e8b4c92e20f2d1d463f7a3b555f9554aa
Artem <rubikkubika@gmail.com>
2025-10-16 20:10:41 +0300

    Update commit info to b20d2

--- backend/src/main/java/com/dobalito/controller/AppController.java
+++ backend/src/main/java/com/dobalito/controller/AppController.java
@@ -32,7 +32,7 @@ public class AppController {
     @GetMapping("/info/commit")
     public ResponseEntity<?> commitInfo() {
         return ResponseEntity.ok(Map.of(
-            "commit", "acbd6"
+            "commit", "b20d2"
         ));
     }
 }
--- frontend-react/src/components/CommitInfo.tsx
+++ frontend-react/src/components/CommitInfo.tsx
@@ -8,7 +8,7 @@ interface CommitInfoProps {
 
 const CommitInfo: React.FC<CommitInfoProps> = ({ className }) => {
   // Hardcoded commit info - update this with each commit/PR to main
-  const commitHash = "acbd6";
+      const commitHash = "b20d2";
 
   const shortCommit = commitHash.substring(0, 5);
 
